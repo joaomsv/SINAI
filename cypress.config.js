7ea24e3bf0cef36253f921ba032ff14478a180d8
Joao Vieira <joaomarcossv@gmail.com>
2022-09-21 21:45:11 -0300

    Added baseUrl to cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www3.epa.gov/carbon-footprint-calculator/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
